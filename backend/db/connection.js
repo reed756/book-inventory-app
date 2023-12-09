fa61aedd95f933bdbb41a99b597a65f7f3c2c024
reedanj <james.r@unitaskr.com>
2023-12-09 20:22:09 +0000

    more dev data

---
+++ backend/db/connection.js
@@ -0,0 +1,28 @@
+import Pool from "pg";
+const ENV = process.env.NODE_ENV || "development";
+
+require("dotenv").config({
+  path: `${__dirname}/../.env.${ENV}`,
+});
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set");
+}
+
+// const config =
+//   ENV === "production"
+//     ? {
+//         connectionString: process.env.DATABASE_URL,
+//         ssl: {
+//           rejectUnauthorized: false,
+//         },
+//       }
+//     : {};
+const config = {};
+
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+
+export default new Pool(config);
---
+++ backend/db/setup.sql
@@ -0,0 +1,5 @@
+DROP DATABASE IF EXISTS book_inventory_test;
+DROP DATABASE IF EXISTS book_inventory;
+
+CREATE DATABASE book_inventory_test;
+CREATE DATABASE book_inventory;
